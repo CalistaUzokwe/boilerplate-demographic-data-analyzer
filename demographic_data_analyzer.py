d98d8979fccb3ac0df55d011bcfd12007758cc29
Calista Ngozi  Uzokwe <calistauzokwe@yahoo.com>
2025-03-28 02:52:16 +0000

    Saving file

--- demographic_data_analyzer.py
+++ demographic_data_analyzer.py
@@ -66,8 +66,9 @@ if print_data:
     print("Country with highest percentage of rich:", highest_earning_country)
     print(f"Highest percentage of rich people in country: {highest_earning_country_percentage}%")
     print("Top occupations in India:", top_IN_occupation)
-
-    return{'race_count': race_count,
+    
+    return
+    {'race_count': race_count,
     'average_age_men': average_age_men,
     'percentage_bachelors': percentage_bachelors,
     'higher_education_rich': higher_education_rich,
